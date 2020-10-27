751a96dd61ac66a33b3d29f0fb217044acad6d48
Justin Cha <justin.chachoe@gmail.com>
2020-10-27 13:38:55 +0700

    git install and first commit

---
+++ crudproject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+import crudapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', crudapp.views.home, name='home'),
+]
