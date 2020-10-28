3f61ff3e929b6179641d117947a2e65076e9cf97
justinchachoe <73210465+justinchachoe@users.noreply.github.com>
2020-10-28 13:15:44 +0700

    new page make

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
+
 from .models import Blog
 from .form import BlogUpdate
 
@@ -7,7 +9,12 @@ from .form import BlogUpdate
 
 def home(request):
     blogs = Blog.objects.order_by('-id')
-    return render(request, 'home.html', {'blogs': blogs})
+    blog_list = Blog.objects.all().order_by('-id')
+    paginator = Paginator(blog_list,3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request,'home.html', {'blogs':blogs,'posts':posts} )
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
